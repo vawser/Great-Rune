c09ed262510a89015dfb063915e76bc52cc1a11f
Vawser <ben.rouncefield@gmail.com>
2022-03-14 17:03:19 +0000

    Weapons + Mimic Tear
    - Fixed the following weapon so they now scale correctly:
     - Regalia of Eochaid
     - Marais Executioner's Sword
     - Rivers of Blood
     - Eleonora's Poleblade
     - Mohgwyn's Sacred Spear
     - Clinging Bone
    
    - Occult weapons will now deal 5% additional damage to Undead and prevent resurrection.
    
    - Mimic Tear: reduced damage multiplier from reinforcement to {1 -> 1.5} (was {1 -> 2.5}).

--- _Localization/_Copy MSGBND to Mod and Game.bat
+++
@@ -1,6 +0,0 @@
-copy /b/v/y "item.msgbnd.dcx" "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game\msg\engus\item.msgbnd.dcx"
-copy /b/v/y "menu.msgbnd.dcx" "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game\msg\engus\menu.msgbnd.dcx"
-
-copy /b/v/y "item.msgbnd.dcx" "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Mods\Great Rune\Mod Files\msg\engus\item.msgbnd.dcx"
-copy /b/v/y "menu.msgbnd.dcx" "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Mods\Great Rune\Mod Files\msg\engus\menu.msgbnd.dcx"
-
--- _Localization/_Repack FMG - Item.bat
+++
@@ -1,1 +0,0 @@
-for %%i in ("item-msgbnd-dcx\GR\data\INTERROOT_win64\msg\engUS\"*.xml) do "Yabber\Yabber.exe" "%%i"
